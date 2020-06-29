1c1b5ee1d989b73959473c4ab656fa822890c5a4
Dan Dumitriu <dandumitriu33@gmail.com>
2020-06-29 20:52:47 +0300

    Add JSON serialization and deserialization successfully

---
+++ SerializationJSON/SerializationJSON/Person.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+using System.Text.Json;
+
+namespace SerializationJSON
+{
+    class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public DateTime BirthDate { get; set; }
+        public Gender PersonGender { get; set; }
+
+        public string Serialize()
+        {
+            return JsonSerializer.Serialize(this);
+        }
+    }
+
+    
+}
--- SerializationJSON/SerializationJSON/Program.cs
+++ SerializationJSON/SerializationJSON/Program.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Runtime.Serialization;
+using System.Text.Json;
+
 
 namespace SerializationJSON
 {
@@ -10,6 +13,21 @@ namespace SerializationJSON
     {
         static void Main(string[] args)
         {
+            Person john = new Person()
+            {
+                Name = "John",
+                Age = 83,
+                BirthDate = new DateTime(1937, 6, 21),
+                PersonGender = Gender.Male
+            };
+
+            var tempJSON = john.Serialize();
+            Console.WriteLine(tempJSON);
+
+            Person john2 = JsonSerializer.Deserialize<Person>(tempJSON);
+            Console.WriteLine($"John2 name: {john2.Name}; John2 age: {john2.Age} John2 gender: {john2.PersonGender}...");
+
         }
+
     }
 }
