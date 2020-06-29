4f0989c361507ed0f754b0457c6e54a1bf102324
Dan Dumitriu <dandumitriu33@gmail.com>
2020-06-29 20:32:46 +0300

    Add initial Console App project

---
+++ SerializationJSON/SerializationJSON/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SerializationJSON
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
